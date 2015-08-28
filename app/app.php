b97b89e6479193351d5bb57edc302a898213f982
Casey Heitz <CaseyHeitz@Caseys-MacBook-Pro.local>
2015-08-28 10:54:21 -0700

    Add index.html.twig

---
+++ app/app.php
@@ -0,0 +1,29 @@
+<?php
+
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Brand.php";
+    require_once __DIR__."/../src/Store.php";
+
+    $app = new Silex\Application();
+    $app['debug'] = true;
+    $server = 'mysql:host=localhost:8889;dbname=shoes';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    use Symfony\Component\HttpFoundation\Request;
+    Request::enableHttpMethodParameterOverride();
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__.'/../views'
+    ));
+
+    //Main page render
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('index.html.twig');
+    });
+
+
+    return $app;
+
+?>
